1a0930a2f5fce0655714cb999af7d7cca5c64e0e
ozer <337706@student.mboutrecht.nl>
2025-10-31 20:50:37 +0100

    Model gevult

--- app/Models/LeverantModel.php
+++ app/Models/LeverantModel.php
@@ -7,4 +7,12 @@ use Illuminate\Support\Facades\DB;
 
 class LeverantModel extends Model
 {
+    public function sp_GetLeverantieInfo($id)
+    {
+        return DB::select('CALL sp_GetLeverantieInfo(?)', [$id]);
+    }
+
+     public function sp_GetLeverancierInfo($id) {
+        return DB::select('CALL sp_GetLeverancierInfo(?)', [$id]);
+    }
 }
