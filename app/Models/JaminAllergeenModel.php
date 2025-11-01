e8f975f8dc4ff9c1d8e31b7c17c0038edc765bbe
ozer <337706@student.mboutrecht.nl>
2025-11-01 10:11:10 +0100

    Allergeen Model kan nu data ophalen

--- app/Models/JaminAllergeenModel.php
+++ app/Models/JaminAllergeenModel.php
@@ -7,6 +7,18 @@ use Illuminate\Support\Facades\DB;
 
 class JaminAllergeenModel extends Model
 {
-    
+    public function getAllergeenData($id)
+    {
+        // Product info
+        $product = DB::select('CALL sp_GetAllergeenProInfo(?)', [$id]);
+        
+        // Allergenen info
+        $allergenen = DB::select('CALL sp_GetAllergeenInfo(?)', [$id]);
+
+        return [
+            'product' => $product[0] ?? null,
+            'allergenen' => $allergenen
+        ];
+    }
 }
 
